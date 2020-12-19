b594a11ac8ac23331d0253934bc455f55c0d4f96
manoharpatel007 <manoharkebhaiya@gmail.com>
2020-12-20 01:41:06 +0530

    dekho ab

--- module/module1.js
+++ module/module1.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/manohar', {useNewUrlParser: true},{ useUnifiedTopology: true } );
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/manohar', {useNewUrlParser: true , useUnifiedTopology: true } );
 
 
 
